0d7f3da484858dbb128373600d5403a3d44682d8
DongHyun Jung <jdh9490@gmail.com>
2023-01-27 23:03:27 +0900

    Feat: Add logout

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '../routes/Home';
 import Auth from '../routes/Auth';
+import Navigation from './Navigation';
+import Profile from '../routes/Profile';
 
 const Router = ({ isLoggedIn }) => {
   return (
     <HashRouter>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/profile" element={<Profile />} />
           </>
         ) : (
           <>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { getAuth, signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
-  return <div>Profile</div>;
+  const auth = getAuth(); // auth 인스턴스
+  const navigate = useNavigate();
+
+  const onLogOutClick = () => {
+    auth.signOut(auth);
+    navigate('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 }
 
 export default Profile;
