5eda8b6e519293d9124c39470cf01d9cfce413e6
DongHyun Jung <jdh9490@gmail.com>
2023-02-10 21:03:47 +0900

    Fix: edit social login function

--- src/routes/Auth/Auth.js
+++ src/routes/Auth/Auth.js
@@ -1,30 +1,32 @@
 import React from 'react';
+import { Button, Icon } from 'semantic-ui-react';
 import {
   getAuth,
   signInWithPopup,
-  GithubAuthProvider,
+  TwitterAuthProvider,
   GoogleAuthProvider,
+  FacebookAuthProvider,
 } from 'firebase/auth';
 import AuthForm from '../../components/authform/AuthForm';
 import './Auth.css';
-import { FcGoogle } from 'react-icons/fc';
-import { FaGithub } from 'react-icons/fa';
 import rectangle1 from '../../assets/Rectangle 30.png';
 import rectangle2 from '../../assets/Rectangle 29.png';
 
 function Auth() {
-  const auth = getAuth(); // auth 인스턴스
-  // google, github 계정으로 로그인
+  const auth = getAuth();
   const onSocialClick = async event => {
     const {
       target: { name },
     } = event;
 
     let provider;
+
     if (name === 'google') {
       provider = new GoogleAuthProvider();
-    } else if (name === 'github') {
-      provider = new GithubAuthProvider();
+    } else if (name === 'twitter') {
+      provider = new TwitterAuthProvider();
+    } else if (name === 'facebook') {
+      provider = new FacebookAuthProvider();
     }
     await signInWithPopup(auth, provider);
   };
@@ -38,12 +40,30 @@ function Auth() {
         <img src={rectangle1} />
       </div>
       <div className="social-login">
-        <button className="google-btn" onClick={onSocialClick} name="google">
-          <FcGoogle className="social-icon" />
-        </button>
-        <button className="github-btn" onClick={onSocialClick} name="github">
-          <FaGithub className="social-icon" />
-        </button>
+        <Button
+          className="social-btn"
+          color="google plus"
+          onClick={onSocialClick}
+          name="google"
+        >
+          <Icon name="google plus" /> Google Plus
+        </Button>
+        <Button
+          className="social-btn"
+          color="twitter"
+          onClick={onSocialClick}
+          name="twitter"
+        >
+          <Icon name="twitter" /> Twitter
+        </Button>
+        <Button
+          className="social-btn"
+          color="facebook"
+          onClick={onSocialClick}
+          name="facebook"
+        >
+          <Icon name="facebook" /> Facebook
+        </Button>
       </div>
     </div>
   );
